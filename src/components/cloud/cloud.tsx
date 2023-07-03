4247fda1dc6e81e373cb3f90320192be8f34a442
YANEXdev <i.vanya.kolupaev@gmail.com>
2023-07-03 23:28:29 +0300

    verstka

--- src/components/cloud/cloud.tsx
+++ src/components/cloud/cloud.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 
-export default function Cloud({ top }: { top?: boolean }) {
+export default function Cloud() {
 
 	return (
 		<div>
-			<Image width={32} height={27} alt="cloud" priority={true} className={`${top ? "toTop" : "toBottom"} small-cloud z-0`} src={"/images/small-sky-bg.png"} />
+			<Image width={32} height={27} alt="cloud" priority={true} className={'small-cloud z-0'} src={"/images/small-sky-bg.png"} />
 		</div>
 	)
 }
--- src/components/cloudLine/cloudLine.tsx
+++ src/components/cloudLine/cloudLine.tsx
@@ -8,9 +8,9 @@ export default function CloudLine({ top }: { top?: boolean }) {
 	const [clouds, setClouds] = useState([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
 
 	return (
-		<div className="flex gap-[73px] z-0 flex-col bg-white min-w-fit">
+		<div className={`flex gap-[73px] z-0 flex-col bg-white min-w-fit ${top ? "toTop" : "toBottom"}`}>
 			{
-				clouds.map((element: any, index: number) => <Cloud key={index} top={top} />)
+				clouds.map((element: any, index: number) => <Cloud key={index} />)
 			}
 		</div>
 	)
