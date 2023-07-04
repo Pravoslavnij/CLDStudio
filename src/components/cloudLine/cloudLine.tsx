d39aae55c4d41b8a65688c386e16c6f48ba017c3
YANEXdev <i.vanya.kolupaev@gmail.com>
2023-07-04 19:59:33 +0300

    new

--- src/components/cloudLine/cloudLine.tsx
+++ src/components/cloudLine/cloudLine.tsx
@@ -5,7 +5,7 @@ import Cloud from "../cloud/cloud";
 
 export default function CloudLine({ top }: { top?: boolean }) {
 
-	const [clouds, setClouds] = useState([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+	const [clouds, setClouds] = useState([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
 
 	return (
 		<div className={`flex gap-[73px] z-0 flex-col bg-white min-w-fit ${top ? "toTop" : "toBottom"}`}>
